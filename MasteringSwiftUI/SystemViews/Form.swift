479344d820fd81d332580efea941a42edbf84f68
김낙진 <nakjin@mail.shinhan.dev>
2022-01-30 16:46:49 +0900

    Form

--- MasteringSwiftUI/SystemViews/Form.swift
+++ MasteringSwiftUI/SystemViews/Form.swift
@@ -24,26 +24,31 @@
 import SwiftUI
 
 struct View_Form: View {
-   @State private var email = ""
-   @State private var password = ""
-   @State private var address = ""
-   @State private var age = 0
-   @State private var receiveEmail = false
-   
-   var body: some View {
-      VStack {
-         TextField("Email", text: $email)
-         SecureField("Password", text: $password)
-         TextField("Address", text: $address)
-         
-         Stepper("Age: \(age)", value: $age)
-         Toggle(isOn: $receiveEmail, label: { Text("Receive Email") })
-      }
-   }
+	@State private var email = ""
+	@State private var password = ""
+	@State private var address = ""
+	@State private var age = 0
+	@State private var receiveEmail = false
+	
+	var body: some View {
+		Form {
+			Section {
+				TextField("Email", text: $email)
+				SecureField("Password", text: $password)
+				TextField("Address", text: $address)
+			}
+			
+			Section {
+				Stepper("Age: \(age)", value: $age)
+				Toggle(isOn: $receiveEmail, label: { Text("Receive Email") })
+			}
+			
+		}
+	}
 }
 
 struct View_Form_Previews: PreviewProvider {
-   static var previews: some View {
-      View_Form()
-   }
+	static var previews: some View {
+		View_Form()
+	}
 }
