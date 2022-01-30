142d81a6e6b23d7c8279fec87d30c76f375542b3
김낙진 <nakjin@mail.shinhan.dev>
2022-01-30 11:46:21 +0900

    Spacer

--- MasteringSwiftUI/SystemViews/Spacer.swift
+++ MasteringSwiftUI/SystemViews/Spacer.swift
@@ -24,41 +24,51 @@
 import SwiftUI
 
 struct View_Spacer: View {
-   var body: some View {
-      VStack(spacing: 0) { // #6
-         HStack {
-            Image(systemName: "suit.heart.fill")
-               .resizable()
-               .frame(width: 70, height: 70)
-               .foregroundColor(.white)
-            
-            // #3
-         }
-         .padding()
-         .background(Color.blue)
-         
-         // #1
-         
-         VStack {
-            // #5
-            
-            Image(systemName: "suit.spade.fill")
-               .resizable()
-               .frame(width: 70, height: 70)
-               .foregroundColor(.white)
-            
-            // #4
-         }
-         .padding()
-         .background(Color.red)
-         
-         // #2
-      }
-   }
+	var body: some View {
+		VStack(spacing: 0) { // #6
+			HStack {
+				Image(systemName: "suit.heart.fill")
+					.resizable()
+					.frame(width: 70, height: 70)
+					.foregroundColor(.white)
+				
+				// #3
+				Spacer()
+			}
+			.padding()
+			.background(Color.blue)
+			
+			// #1
+//			Spacer(minLength: 50)
+			Spacer()
+				.frame(height: 50)
+			
+			VStack {
+				// #5
+//				Spacer()
+				Image(systemName: "suit.spade.fill")
+					.resizable()
+					.frame(width: 70, height: 70)
+					.foregroundColor(.white)
+				
+				// #4
+				
+			}
+			.padding()
+			.background(Color.red)
+			
+			// #2
+//			Spacer()
+//			Spacer(minLength: 50)
+			Spacer()
+				.frame(height: 20)
+			Spacer()
+		}
+	}
 }
 
 struct View_Spacer_Previews: PreviewProvider {
-   static var previews: some View {
-      View_Spacer()
-   }
+	static var previews: some View {
+		View_Spacer()
+	}
 }
