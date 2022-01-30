fd39731668e0d801c3351c14aee45e9de97b5b4a
김낙진 <nakjin@mail.shinhan.dev>
2022-01-30 16:31:48 +0900

    Group

--- MasteringSwiftUI/SystemViews/Group.swift
+++ MasteringSwiftUI/SystemViews/Group.swift
@@ -24,13 +24,40 @@
 import SwiftUI
 
 struct View_Group: View {
-   var body: some View {
-      Text("Hello, World!")
-   }
+	var body: some View {
+		VStack {
+			Text("Hello, World!")
+			Text("Hello, World!")
+			Text("Hello, World!")
+			Text("Hello, World!")
+			Text("Hello, World!")
+			Text("Hello, World!")
+			Text("Hello, World!")
+
+			Group {
+				Text("Hello, World!")
+				Text("Hello, World!")
+				Text("Hello, World!")
+				Text("Hello, World!")
+				Text("Hello, World!")
+				Text("Hello, World!")
+
+				Group {
+					Text("Hello, World!")
+					Text("Hello, World!")
+					Text("Hello, World!")
+					Text("Hello, World!")
+					Text("Hello, World!")
+					Text("Hello, World!")
+				}
+			}
+			
+		}
+	}
 }
 
 struct View_Group_Previews: PreviewProvider {
-   static var previews: some View {
-      View_Group()
-   }
+	static var previews: some View {
+		View_Group()
+	}
 }
