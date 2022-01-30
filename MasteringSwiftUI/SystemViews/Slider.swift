1f1de9dbe78aa67020e7301b85efafe80b6db8a6
김낙진 <nakjin@mail.shinhan.dev>
2022-01-30 15:15:34 +0900

    Slider

--- MasteringSwiftUI/SystemViews/Slider.swift
+++ MasteringSwiftUI/SystemViews/Slider.swift
@@ -24,28 +24,74 @@
 import SwiftUI
 
 struct View_Slider: View {
-   @State private var r = 0.0
-   @State private var g = 0.0
-   @State private var b = 0.0
-   
-   var color: Color {
-      Color(red: r / 255, green: g / 255, blue: b / 255)
-   }
-   
-   var body: some View {
-      VStack {
-         Image(systemName: "smiley.fill")
-            .resizable()
-            .foregroundColor(color)
-            .frame(width: 250, height: 250)
-         
-         
-      }
-   }
+	@State private var r = 0.0
+	@State private var g = 0.0
+	@State private var b = 0.0
+	
+	var color: Color {
+		Color(red: r / 255, green: g / 255, blue: b / 255)
+	}
+	
+	var body: some View {
+		VStack {
+			Image(systemName: "smiley.fill")
+				.resizable()
+				.foregroundColor(color)
+				.frame(width: 250, height: 250)
+			
+			HStack {
+				Text("R")
+				Slider(value: $r, in: 0 ... 255, step: 1, label: {
+					EmptyView()
+				}, onEditingChanged: {
+					print($0)
+				})
+				HStack { // 텍스트가 우측에 안붙고 가운데정렬로 있는게 어색해서 Rgb 세줄이 모두 우측에 붙게 하기위해 하나 더 덮어쓴다.
+					Spacer()
+					Text("\(Int(r))")
+				}
+				.frame(width: 40)
+			}
+			.padding()
+			.foregroundColor(.red)
+			
+			HStack {
+				Text("G")
+				Slider(value: $g, in: 0 ... 255, step: 1, label: {
+					EmptyView()
+				}, onEditingChanged: {
+					print($0)
+				})
+				HStack {
+					Spacer()
+					Text("\(Int(g))")
+				}
+				.frame(width: 40)
+			}
+			.padding()
+			.foregroundColor(.green)
+			
+			HStack {
+				Text("B")
+				Slider(value: $b, in: 0 ... 255, step: 1, label: {
+					EmptyView()
+				}, onEditingChanged: {
+					print($0)
+				})
+				HStack {
+					Spacer()
+					Text("\(Int(b))")
+				}
+				.frame(width: 40)
+			}
+			.padding()
+			.foregroundColor(.blue)
+		}
+	}
 }
 
 struct View_Slider_Previews: PreviewProvider {
-   static var previews: some View {
-      View_Slider()
-   }
+	static var previews: some View {
+		View_Slider()
+	}
 }
