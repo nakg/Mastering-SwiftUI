f7c040fa81cb0df1cc06f307cf08fff402c32084
김낙진 <nakjin@mail.shinhan.dev>
2022-02-01 21:38:25 +0900

    TabView

--- MasteringSwiftUI/Navigation/Scene/ImageScene.swift
+++ MasteringSwiftUI/Navigation/Scene/ImageScene.swift
@@ -24,29 +24,35 @@
 import SwiftUI
 
 struct ImageScene: View {
-   var imageName: String
-   var color: Color
-   
-      
-   var body: some View {
-      VStack {
-         Spacer()
-         
-         Image(systemName: imageName)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 200, height: 200)
-            .foregroundColor(color)
-
-         
-         Spacer()
-      }
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-   }
+	var imageName: String
+	var color: Color
+	
+	@Binding var selectedIndex: Int
+	
+	var body: some View {
+		VStack {
+			Spacer()
+			
+			Image(systemName: imageName)
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(width: 200, height: 200)
+				.foregroundColor(color)
+			
+			Button(action: {
+				self.selectedIndex = 2
+			}, label: {
+				Text("Last Tab")
+			})
+			
+			Spacer()
+		}
+		.frame(maxWidth: .infinity, maxHeight: .infinity)
+	}
 }
 
 struct ImageScene_Previews: PreviewProvider {
-   static var previews: some View {
-      ImageScene(imageName: "circle", color: Color.red)
-   }
+	static var previews: some View {
+		ImageScene(imageName: "circle", color: Color.red, selectedIndex: .constant(0))
+	}
 }
--- MasteringSwiftUI/Navigation/TabView.swift
+++ MasteringSwiftUI/Navigation/TabView.swift
@@ -24,19 +24,34 @@
 import SwiftUI
 
 struct Nav_TabView: View {
-   @State private var selectedIndex = 0
-   
-   var body: some View {
-      VStack {
-         ImageScene(imageName: "star", color: Color.red)
-         ImageScene(imageName: "heart", color: Color.green)
-         ImageScene(imageName: "play", color: Color.blue)
-      }
-   }
+	@State private var selectedIndex = 0
+	
+	var body: some View {
+		TabView(selection: $selectedIndex) {
+			ImageScene(imageName: "star", color: Color.red, selectedIndex: $selectedIndex)
+				.tabItem {
+					Image(systemName: "star")
+					Text("Star \(selectedIndex)")
+				}
+				.tag(0)
+			ImageScene(imageName: "heart", color: Color.green, selectedIndex: $selectedIndex)
+				.tabItem {
+					Image(systemName: "heart")
+					Text("Heart")
+				}
+				.tag(1)
+			ImageScene(imageName: "play", color: Color.blue, selectedIndex: $selectedIndex)
+				.tabItem {
+					Image(systemName: "play")
+					Text("Play")
+				}
+				.tag(2)
+		}
+	}
 }
 
 struct Nav_TabView_Previews: PreviewProvider {
-   static var previews: some View {
-      Nav_TabView()
-   }
+	static var previews: some View {
+		Nav_TabView()
+	}
 }
