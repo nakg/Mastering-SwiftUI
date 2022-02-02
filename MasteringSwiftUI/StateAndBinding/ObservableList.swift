7a1ed61892570f1631cbe92e62b3c836276b463e
김낙진 <nakjin@mail.shinhan.dev>
2022-02-02 12:00:28 +0900

    Observable Object

--- MasteringSwiftUI/StateAndBinding/ObservableList.swift
+++ MasteringSwiftUI/StateAndBinding/ObservableList.swift
@@ -23,37 +23,51 @@
 
 import SwiftUI
 
+class SharedList: ObservableObject { // 이렇게 선언된 클래스를 프로토콜 이름처럼 ObservableObject라고 부른다. 이제 이름처럼 감시가 가능한 객체가 되었다.
+	var title = "" // 값이 바뀌어도 새로운 값을 방출하지 않는다. 이 속성을 바꾸는 것만으로는 뷰가 업데이트 되지 않는다.
+	@Published var list = [String]() // 속성앞에 @Published를 추가하면 배열에 새로운요소가 추가되거나 삭제되는 시점마다 새로운 배열을 방출한다. 이 리스트를 리스트나 포이치에서 사용하면 항사 새로운 목록이 된다.
+}
 
 struct ObservableList: View {
-   @State private var value: String = ""
-   
-   
-   var body: some View {
-      VStack {
-         
-         HStack {
-            TextField("Input", text: $value)
-               .textFieldStyle(RoundedBorderTextFieldStyle())
-               .padding()
-            
-            Button(action: {
-               
-            }, label: {
-               Text("Add To List")
-            })
-               .padding()
-         }
-        
-         
-         
-         Spacer()
-      }
-      .padding()
-   }
+	@State private var value: String = ""
+	
+	@ObservedObject var sharedList = SharedList() // class instance를 저장. 이렇게 선언하면 인스턴스에 포함된 publsihed속성이 새로운 값을 방출할 때 마다 뷰를 자동으로 업데이트한다.
+	
+	var body: some View {
+		VStack {
+			Text(sharedList.title)
+				.font(.largeTitle)
+			
+			HStack {
+				TextField("Input", text: $value)
+					.textFieldStyle(RoundedBorderTextFieldStyle())
+					.padding()
+				
+				Button(action: {
+					self.sharedList.title = "Observable List"
+					self.sharedList.list.insert(self.value, at: 0)
+					self.value = ""
+					
+				}, label: {
+					Text("Add To List")
+				})
+					.padding()
+				
+			}
+			
+			List(sharedList.list, id: \.self) { item in
+				Text(item)
+				
+			}
+	
+			Spacer()
+		}
+		.padding()
+	}
 }
 
 struct ObservableList_Previews: PreviewProvider {
-   static var previews: some View {
-      ObservableList()
-   }
+	static var previews: some View {
+		ObservableList()
+	}
 }
--- MasteringSwiftUI/StateAndBinding/TwoWayConnection.swift
+++ MasteringSwiftUI/StateAndBinding/TwoWayConnection.swift
@@ -24,25 +24,43 @@
 import SwiftUI
 
 struct TwoWayConnection: View {
-   var value: String = "Hello"
-   
-   var body: some View {
-      VStack(spacing: 70) {
-         Text(value)
-            .font(.largeTitle)
-                  
-         Button(action: {
-            
-         }, label: {
-            Text("Update")
-         })
-         .padding()
-      }
-   }
+	@State private var value: String = "Hello"
+	
+	var body: some View {
+		VStack(spacing: 70) {
+			Text(value)
+				.font(.largeTitle)
+			
+			TextField("Input", text: $value)
+				.padding()
+			
+			BindingButton(value: $value)
+		}
+	}
+}
+
+struct BindingButton: View {
+	@Binding var value: String // 다른데서 사용해야하기에 private하지 않는다.실제 저장은 State Variable이 있는 뷰가 소유하고 있으므로, 초기화까진 필요없다.
+	
+	var body: some View {
+		Button(action: {
+			self.value = "Hi~~~"
+		}, label: {
+			Text(value) // 바인딩은 state variable과 two way connection으로 연결되기에,TwoWayConnection의 텍스트필드를 수정해도 바로 반영된다.
+		})
+			.padding()
+	}
 }
 
 struct TwoWayConnection_Previews: PreviewProvider {
-   static var previews: some View {
-      TwoWayConnection()
-   }
+	
+	
+	static var previews: some View {
+		Group {
+			TwoWayConnection()
+			BindingButton(value: .constant("Test")) // binding을 요구하는 미리보기 화면에서, 혹은 프로토타입에서 제한적으로 constant를 쓴다.. bindingButton의 binding value를 프리뷰에선 가져올 방도가 없다. 그래서 임시로 쓰는 것.
+		}
+	}
 }
+
+
